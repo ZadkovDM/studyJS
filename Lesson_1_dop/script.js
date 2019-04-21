ffc4bda0cb4539c928d969961d35b36a39bd80b4
Dmitriy <gaucho92@mail.ru>
2019-04-22 01:11:25 +0200

    Мой третий комит

---
+++ Lesson_1_dop/script.js
@@ -0,0 +1,6 @@
+var num = 33721;
+var x = 3 * 3 * 7 * 2 * 1;
+console.log(x);
+x **= 3;
+console.log(x);
+alert( 20 );
---
+++ Lesson_1_dop/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+var num = 33721;
+var x = 3 * 3 * 7 * 2 * 1;
+console.log(x);
+x **= 3;
+console.log(x);
