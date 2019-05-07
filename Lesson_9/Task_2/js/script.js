d3f394585b896e70d9705b309985a577ec367a2f
Dmitriy <gaucho92@mail.ru>
2019-05-07 11:13:53 +0200

    Исправление Lesson_9

--- Lesson_9/Task_2/js/script.js
+++ Lesson_9/Task_2/js/script.js
@@ -1,8 +1,9 @@
 let age = document.getElementById('age');
 
 function showUser(surname, name) {
-	this.value = age.value;
 	alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
 }
 
-showUser('Столыпин', 'Петр');
+showUser.call(age, 'Столыпин', 'Петр');
+
+// showUser('Столыпин', 'Петр');
